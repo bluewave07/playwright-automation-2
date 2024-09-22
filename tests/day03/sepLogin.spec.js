b0a2d3a79dc2d9cc6cb692f47e6158e4e3704b3e
blauwave <maviwave@gmail.com>
2024-09-22 21:57:49 +0200

    day 3 codes

---
+++ tests/day03/sepLogin.spec.js
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test';
+
+test('SEP Login', async ({ page }) => {
+
+    const code = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString("base64");
+
+    await page.setExtraHTTPHeaders({
+        Authorization: `Basic ${code}`
+    });
+
+    await page.goto(process.env.SEP_URL);
+
+    await page.waitForTimeout(5000);
+
+    expect(await page.title()).toContain("Checkout");
+  
+});
+
