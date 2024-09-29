85ee66526096b352bd885b7da791051c0fa1dd3b
blauwave <maviwave@gmail.com>
2024-09-29 18:18:25 +0200

    comments

--- tests/day02/playwrightPractice.spec.js
+++ tests/day02/playwrightPractice.spec.js
@@ -1,28 +1,28 @@
-import { test } from '@playwright/test';
+// import { test } from '@playwright/test';
 
-test('Youtube Search', async ({ page }) => {
-   await page.goto("https://www.youtube.com/");
+// test('Youtube Search', async ({ page }) => {
+//    await page.goto("https://www.youtube.com/");
 
 
-   const element=  page.locator("(//div[@class='yt-spec-touch-feedback-shape__fill'])[8]");
+//    const element=  page.locator("(//div[@class='yt-spec-touch-feedback-shape__fill'])[8]");
 
-   await element.scrollIntoViewIfNeeded();
+//    await element.scrollIntoViewIfNeeded();
    
-   element.click();
+//    element.click();
 
-   let searchBox = page.locator("//input[@id='search']");
+//    let searchBox = page.locator("//input[@id='search']");
 
-   await searchBox.click();
+//    await searchBox.click();
 
-   await searchBox.fill('Cydeo');
+//    await searchBox.fill('Cydeo');
 
-   await searchBox.press("Enter");
+//    await searchBox.press("Enter");
 
 
-   let firstResult = page.locator("(//a[@id='video-title'])[1]");
+//    let firstResult = page.locator("(//a[@id='video-title'])[1]");
 
-   await firstResult.click();
+//    await firstResult.click();
 
-   // await page.waitForTimeout(10000);
+//    // await page.waitForTimeout(10000);
 
-});
+// });
